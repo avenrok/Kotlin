2a079a9592e01f5ccc8fa9d79d9cad904043dc81
avenrok <108291724+avenrok@users.noreply.github.com>
2022-07-02 23:23:04 +0700

    Add files via upload

--- decription.kt
+++ decription.kt
@@ -1,17 +1,22 @@
 fun main() {
     val word: String = "F2p)v\"y233{0->c}ttelciFc"
-    val firstPart = word.take(12)
-    val lastPart = word.takeLast(12)
-    // Обработка первой половины
-    val sdvigFirst = firstPart.map{ char -> char + 1 }.joinToString("")
-    val zamenaOneFirst = sdvigFirst.replace("4","u")
-    val sdvigFirstTwo = zamenaOneFirst.map{ char -> char - 3 }.joinToString("")
-    val endFirst = sdvigFirstTwo.replace("0","o")
-    //Обработка второй половины
-    val reversLast = lastPart.reversed()
-    val sdvigLast = reversLast.map{ char -> char - 4 }.joinToString("")
-    val endLast = sdvigLast.replace("_"," ")
+    algoritmForFirstPart(word)
+    algoritmForlastPart(word)
+}
+fun algoritmForFirstPart(word: String){
+    val operations = word.take(12)
 
+    val endFirst = operations.map{ char -> char + 1 }.joinToString("")
+        .replace("4","u")
+        .map{ char -> char - 3 }.joinToString("")
+        .replace("0","o")
     println(endFirst)
+}
+fun algoritmForlastPart(word: String){
+    val lastPart = word.takeLast(12)
+
+    val endLast = lastPart.reversed()
+        .map{ char -> char - 4 }.joinToString("")
+        .replace("_"," ")
     println(endLast)
 }
